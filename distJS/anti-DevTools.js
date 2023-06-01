2d245a89e7e6ff410fd8dd21129e758ca4f2a68f
math.μ ✯ <96264224+matthewpenkala@users.noreply.github.com>
2023-06-01 08:21:39 -0700

    Update anti-DevTools.js

--- distJS/anti-DevTools.js
+++ distJS/anti-DevTools.js
@@ -1,47 +1,15 @@
-javascript: (async () => {
-    setInterval(function () {
-        checkingOK()
-    }, 1000);
-    var checkingOK = function () {
-        function doCheckFWD(a) {
-            if (("" + a / a)["length"] !== 1 || a === 0) {
-                (function () {} ["constructor"]("debugger")())
-            } else {
-                setTimeout((() => {
-                    e: void 0;console.clear();e: void 0
-                }), 100);
-            }
-            doCheckFWD(++a)
+async function executeFunction() {
+    const scrollHeightObserver = value => {
+        if (Number.isNaN(value / value) || value === 0) {
+            debugger;
+        } else {
+            setTimeout(() => {
+                console.clear();
+            }, 100);
         }
-        setTimeout(doCheckFWD, 500);
-        setTimeout(() => {
-            try {
-                doCheckFWD(0)
-            } catch (err) {
-                function n(t, e) {
-                    var a = r();
-                    return (n = function (n, r) {
-                        return a[n -= 219]
-                    })(t, e)
-                }
-
-                function r() {
-                    var n = ["82992WueBNP", "6252140zZSflh", "432SIfNzf", "367130eLihdQ", "36njdaTb", "26292lpCeSR", "11qpvALn", "2EVZrml", "305Ctutpz", "574236TrKpQm", "3464EyJOFg", "clear", "1441601IYFSwM"];
-                    return (r = function () {
-                        return n
-                    })()
-                }(function () {
-                    for (var t = n, e = r();;) try {
-                        if (649351 == -parseInt(t(231)) * (parseInt(t(222)) / 2) + parseInt(t(220)) / 3 + -parseInt(t(225)) / 4 * (parseInt(t(223)) / 5) + parseInt(t(219)) / 6 * (parseInt(t(227)) / 7) + parseInt(t(228)) / 8 * (parseInt(t(230)) / 9) + -parseInt(t(229)) / 10 + -parseInt(t(221)) / 11 * (parseInt(t(224)) / 12)) break;
-                        e.push(e.shift())
-                    } catch (n) {
-                        e.push(e.shift())
-                    }
-                })(), setTimeout(() => {
-                    console[n(226)]()
-                }, 5e3);
-            }
-        }, 500)
+        scrollHeightObserver(++value);
     };
-    checkingOK()
-})();
+    setTimeout(() => scrollHeightObserver(0), 500);
+}
+setInterval(executeFunction, 1000);
+executeFunction();
